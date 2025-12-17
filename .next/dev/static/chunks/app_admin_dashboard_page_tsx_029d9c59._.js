2893c619b7a1cea9ef11a3959294f6aeba67971d
captain nork <138743746+norkart1@users.noreply.github.com>
2025-12-17 12:18:43 +0000

    Updated

--- .next/dev/static/chunks/app_admin_dashboard_page_tsx_029d9c59._.js
+++ .next/dev/static/chunks/app_admin_dashboard_page_tsx_029d9c59._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_03d23c61._.js"
+  "static/chunks/_6aec20cc._.js"
 ],
     source: "dynamic"
 });
